2b3d779021459a31082e45c89de43ef1a58ea82b
FernandoMotta <fernandosantosmotta@gmail.com>
2022-10-06 20:57:47 -0300

    adiciona firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-realtime-database',
+    appId: '1:1057478022201:web:15a6e2af6eedfa8310a9e8',
+    databaseURL: 'https://angular-realtime-database-default-rtdb.firebaseio.com',
+    storageBucket: 'angular-realtime-database.appspot.com',
+    apiKey: 'AIzaSyD6ssGmYAck6-gfcutRmX-eD1INdWVhiMQ',
+    authDomain: 'angular-realtime-database.firebaseapp.com',
+    messagingSenderId: '1057478022201',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'angular-realtime-database',
+    appId: '1:1057478022201:web:15a6e2af6eedfa8310a9e8',
+    databaseURL: 'https://angular-realtime-database-default-rtdb.firebaseio.com',
+    storageBucket: 'angular-realtime-database.appspot.com',
+    apiKey: 'AIzaSyD6ssGmYAck6-gfcutRmX-eD1INdWVhiMQ',
+    authDomain: 'angular-realtime-database.firebaseapp.com',
+    messagingSenderId: '1057478022201',
+  }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
